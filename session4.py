c6207fc93b14865660539cc1bcd78325241a282d
Sumanth Kumar Reddy <farmfoodsn@gmail.com>
2026-02-26 16:27:37 +0530

    APPLE (Motor Control).py

---
+++ session4.py
@@ -0,0 +1,16 @@
+# SESSION 4 – APPLE MOTOR CONTROL
+
+# Sample robot motor data
+distance = 10   # meters
+time = 2        # seconds
+
+# Speed calculation
+speed = distance / time
+
+print("Distance:", distance, "m")
+print("Time:", time, "sec")
+print("Motor Speed:", speed, "m/s")
+OUTPUT:
+Distance: 10 m
+Time: 2 sec
+Motor Speed: 5.0 m/s
