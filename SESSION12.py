0e62d2eb87bf7c8c1c210ad2526f5e653d0329d1
Sumanth Kumar Reddy <farmfoodsn@gmail.com>
2026-03-08 15:27:14 +0530

    GOOGLE (Place Recognition)  .py

---
+++ SESSION12.py
@@ -0,0 +1,10 @@
+similarity = 0.95
+
+if similarity > 0.9:
+    recognized = True
+    print("Place Recognized")
+else:
+    recognized = False
+    print("Place Not Recognized")
+  output:
+Place Recognized
