2b26044a424b180d5c9aebaa0ecd38d63e75bb78
Sumanth Kumar Reddy <farmfoodsn@gmail.com>
2026-03-08 16:10:50 +0530

    AMAZON (Go-To-Goal).py

---
+++ SESSION27.py
@@ -0,0 +1,14 @@
+import math
+
+# Start position
+start = (0, 0)
+
+# Goal position
+goal = (6, 8)
+
+# Distance calculation
+distance = math.sqrt((goal[0] - start[0])**2 + (goal[1] - start[1])**2)
+
+print("Distance =", int(distance), "m
+ouput:
+Distance = 10 m
