00bd68bd503e494c7dd6eae5e0176762ae820b2f
Sumanth Kumar Reddy <farmfoodsn@gmail.com>
2026-03-08 16:39:34 +0530

    AMAZON (Patrolling).py

---
+++ SESSION37.py
@@ -0,0 +1,16 @@
+# Waypoint Patrolling Example
+
+# List of patrol waypoints
+waypoints = [(0,0), (2,2), (4,4), (2,4)]
+
+# Patrol loop
+for point in waypoints:
+    print("Moving to waypoint:", point)
+
+print("Continuous Patrol")
+output:
+Moving to waypoint: (0, 0)
+Moving to waypoint: (2, 2)
+Moving to waypoint: (4, 4)
+Moving to waypoint: (2, 4)
+Continuous Patrol
