544791def84427c024bf49a63e9c85d45e840655
Sumanth Kumar Reddy <farmfoodsn@gmail.com>
2026-03-08 16:29:23 +0530

    AMAZON (Wander Bot).py

---
+++ SESSION33.py
@@ -0,0 +1,11 @@
+import random
+
+# Possible movement directions
+directions = ["Forward", "Left", "Right", "Backward"]
+
+# Randomly choose a new direction
+new_direction = random.choice(directions)
+
+print("Direction Updated:", new_direction)
+output:
+Direction Updated: Forward
